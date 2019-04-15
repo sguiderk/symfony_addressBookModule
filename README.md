8b41be58675ade4da6d1b1ef02f2ebcc2c94fd9a
adil <adilelgnn@gmail.com>
2019-04-15 18:14:58 +0100

    adresse book test completed

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ lillydoo
 ========
 
 A Symfony project created on April 3, 2019, 6:17 pm.
+Could not open input file: echo
--- tests/AppBundle/Controller/IndexControllerTest.php
+++ tests/AppBundle/Controller/IndexControllerTest.php
@@ -6,6 +6,7 @@ use Symfony\Component\Validator\Constraints\Length;
 
 class IndexControllerTest extends WebTestCase
 {
+
     
     public function testIndex()
     {
@@ -16,8 +17,6 @@ class IndexControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         
         $this->assertEquals(1,$crawler->filter("p.count")->count());
-        
-        
         
     }
     
@@ -37,8 +36,46 @@ class IndexControllerTest extends WebTestCase
         
         $this->assertGreaterThan(0, $crawler->filter(".form-error-message")->count());
         
-        $form->setValues([
-            "appbundle_person[lastName]"=> "first name",
+        $form->setValues($this->getContactTestExemple());
+        
+        $crawler = $client->submit($form);
+        
+        $this->assertTrue($client->getResponse()->isRedirect("/"));
+        $this->assertEquals(0,$crawler->filter(".form-error-message")->count());
+        
+    }
+    
+    public function testEdit()
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request("GET", "/edit/1");
+        
+        if(!$client->getResponse()->isNotFound())
+        {
+            $form = $crawler->selectButton("save_contact")->form();
+            
+            $form->setValues($this->getContactTestExemple());
+            
+            $crawler = $client->submit($form);
+            
+            $this->assertTrue($client->getResponse()->isRedirection());
+            
+        }
+        
+    }
+    
+    public function testDelete()
+    {
+        
+    }
+    
+    private function getContactTestExemple()
+    {
+        return [
+            "appbundle_person[firstName]"=> "first name",
+            
+            "appbundle_person[lastName]"=> "last name",
             
             "appbundle_person[address]"=> "adresse fake",
             
@@ -48,22 +85,15 @@ class IndexControllerTest extends WebTestCase
             
             "appbundle_person[country]"=> "MA",
             
-            "appbundle_person[phoneNumber]"=>"0644484254",
+            "appbundle_person[phoneNumber]"=>"0612345678",
             
-            "appbundle_person[birthday][month]"=> "01",
+            "appbundle_person[birthday][month]"=> "1",
             
-            "appbundle_person[birthday][day]"=>"06",
+            "appbundle_person[birthday][day]"=>"6",
             
             "appbundle_person[birthday][year]"=>"1988",
             
-            "appbundle_person[email]"=> "adil@gmail.com"
-        ]);
-        
-        $crawler = $client->submit($form);
-    }
-    
-    public function testEdit()
-    {
-        
+            "appbundle_person[email]"=> md5(mt_rand(110,10000))."@gmail.com"
+        ];
     }
 }
